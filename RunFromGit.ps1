e97cbcf21bfd38fa506c15c30ce19c89457ec31a
David <david.nauendorf@gmail.com>
2020-07-29 19:51:27 +0800

    wip

--- RunFromGit.ps1
+++ RunFromGit.ps1
@@ -1,16 +1,24 @@
-﻿$res=Invoke-WebRequest "https://api.github.com/repos/nauendorf/helloworld/contents"
-$res.Content|ConvertFrom-Json
+﻿# Report Meta
+(Invoke-WebRequest "https://api.github.com/repos/nauendorf/helloworld").content|ConvertFrom-Json
 
-(Invoke-WebRequest "https://raw.githubusercontent.com/Nauendorf/HelloWorld/master/HellowWorld.ps1").content
+# Report contents
+$files=(Invoke-WebRequest "https://api.github.com/repos/nauendorf/helloworld/contents/").Content|ConvertFrom-Json
+$files[0].sha
 
 
-(curl ((curl "https://api.github.com/repos/nauendorf/helloworld/contents").content|ConvertFrom-Json).download_url).content
+# Commits
+(Invoke-WebRequest "https://api.github.com/repos/nauendorf/helloworld/commits").Content|ConvertFrom-Json
 
-
+# File contents
 $owner='Nauendorf'
 $repo='HelloWorld'
 $branch='master'
 $file='HelloWorld.ps1'
 Invoke-Expression $((curl "https://raw.githubusercontent.com/$owner/$repo/$branch/$file").Content)
-&{$((curl "https://raw.githubusercontent.com/$owner/$repo/$branch/$file").Content)}
+
+
+# Junk
+(Invoke-WebRequest "https://api.github.com/repos/nauendorf/helloworld").content|ConvertFrom-Json
+(Invoke-WebRequest "https://raw.githubusercontent.com/Nauendorf/HelloWorld/master/HelloWorld.ps1").content
+(curl ((curl "https://api.github.com/repos/nauendorf/helloworld/contents").content|ConvertFrom-Json).download_url).content
 
