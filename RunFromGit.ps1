1904c1547d71804bc68b26992f67d48260c79c6f
David <david.nauendorf@gmail.com>
2020-07-29 19:10:15 +0800

    initial

---
+++ RunFromGit.ps1
@@ -0,0 +1,16 @@
+﻿$res=Invoke-WebRequest "https://api.github.com/repos/nauendorf/helloworld/contents"
+$res.Content|ConvertFrom-Json
+
+(Invoke-WebRequest "https://raw.githubusercontent.com/Nauendorf/HelloWorld/master/HellowWorld.ps1").content
+
+
+(curl ((curl "https://api.github.com/repos/nauendorf/helloworld/contents").content|ConvertFrom-Json).download_url).content
+
+
+$owner='Nauendorf'
+$repo='HelloWorld'
+$branch='master'
+$file='HelloWorld.ps1'
+Invoke-Expression $((curl "https://raw.githubusercontent.com/$owner/$repo/$branch/$file").Content)
+&{$((curl "https://raw.githubusercontent.com/$owner/$repo/$branch/$file").Content)}
+
